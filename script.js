a5cfd1bd79fa6cdb4423881d42015e3436798982
manshiginodia <manshiginodia@gmail.com>
2022-07-06 16:00:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var systemGuess = Math.floor(Math.random()*100)+1
+var guess =0
+function guessNumber()
+{
+    var num = document.getElementById('in').value
+    if(num==='' || num>100 || num<1)
+    {
+        document.getElementById('out1').innerText = 'Input is not valid'
+    }
+    else
+    {
+        guess++
+        var num =parseInt(document.getElementById('in').value)
+        if(num === systemGuess)
+        {
+            document.getElementById('out1').innerText = 'Got the Answer'
+        }
+        else if(num> systemGuess)
+        {
+            document.getElementById('out1').innerText = 'Guess a smaller number'
+        }
+        else
+        {
+            document.getElementById('out1').innerText = 'Guess a greater number'
+        }
+        document.getElementById('out2').innerText = 'Number of guesses: ' +guess
+    }
+}
